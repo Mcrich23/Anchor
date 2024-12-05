9a15b8d24c3d2b860a9801e963f50b134c84f77b
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-12-04 23:13:25 -0800

    code cleanup

--- Anchor.swiftpm/SemiCircle.swift
+++ Anchor.swiftpm/SemiCircle.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Morris Richman on 12/4/24.
 //
-// Based on https://bigmountainstudio.wordpress.com/2020/07/26/394/
 
 import SwiftUI
 
@@ -16,7 +15,7 @@ struct SemiCircle: View {
             .fill(Color.clear)
                 .overlay(alignment: .top, content: {
                     if let size {
-                        StretchableCircle()
+                        Ellipse()
                             .trim(from: 0.5, to: 1)
                             .size(width: size.width, height: size.height*2)
                             .fill(.primary)
@@ -28,13 +27,5 @@ struct SemiCircle: View {
                 size = newValue
             }
 
-    }
-}
-
-struct StretchableCircle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.addEllipse(in: rect) // Creates an ellipse that fits the given rectangle
-        return path
     }
 }
